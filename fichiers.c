f143122ee7e91bb698ffe52c630cbd6171e6428c
razak <razak.mohamed@outlook.fr>
2022-05-11 18:05:08 +0200

    Initial commit

---
+++ fichiers.c
@@ -0,0 +1,5 @@
+//
+// Created by razak on 11/05/2022.
+//
+
+#include "fichiers.h"
---
+++ fichiers.h
@@ -0,0 +1,8 @@
+//
+// Created by razak on 11/05/2022.
+//
+
+#ifndef FIZZBUZZ_FICHIERS_H
+#define FIZZBUZZ_FICHIERS_H
+
+#endif //FIZZBUZZ_FICHIERS_H
